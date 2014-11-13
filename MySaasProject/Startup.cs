1e868553016eabe82de76ab87054a258ed62fe7c
Pedro Alonso Ferrer <pedro.paf@gmail.com>
2014-11-12 20:22:21 -0500

    Initial Commit

---
+++ MySaasProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MySaasProject.Startup))]
+namespace MySaasProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
